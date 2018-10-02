3070d96decedd176f67da2d8dc1b5355b1bdd87a
Musa Beni <mbeni@softnet.co.tz>
2018-10-02 16:15:22 +0300

    Initial Commit

---
+++ todo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  todo
+//
+//  Created by Musa Beni on 02/10/2018.
+//  Copyright © 2018 Musa Beni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
