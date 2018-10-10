b27fa285bccbd430ad7df14ce27fb1c73f64b5bc
Musa Beni <mbeni@softnet.co.tz>
2018-10-10 10:08:05 +0300

    convert to mvc pattern

--- todo/Controller/TodoListViewController.swift
+++ todo/Controller/TodoListViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["first item","Second item","third item"]
+    var itemArray = [Items] ()
     
     let defaults = UserDefaults.standard
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        if let item = defaults.array(forKey: "toDolistArray") as? [String]{
-            itemArray = item
+//        if let item = defaults.array(forKey: "toDolistArray") as? [String]{
+//            itemArray = item
+//        }
+        
+        let newItem = Items()
+        newItem.title = "musa beni"
+//        newItem.done = true
+        itemArray.append(newItem)
+        
+        let newItem2 = Items()
+        newItem2.title = "namana musa"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Items()
+        newItem3.title = "Dhureha"
+        itemArray.append(newItem3)
+        
+        if let items = defaults.array(forKey: "toDolistArray") as? [Items]{
+            itemArray = items
         }
+        
         
     }
     
@@ -29,22 +49,45 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let celles = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell", for: indexPath)
+        celles.textLabel?.text = itemArray[indexPath.row].title
         
-        return cell
+        celles.accessoryType = itemArray[indexPath.row].done ? .checkmark : .none
+        
+//        if itemArray[indexPath.row].done == true {
+//            celles.accessoryType = .checkmark
+//        }else{
+//            celles.accessoryType = .none
+//        }
+        
+        
+        
+        
+        return celles
        
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 //        print(itemArray[indexPath.row])
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+//        if itemArray[indexPath.row].done == false{
+//            itemArray[indexPath.row].done = true
+//        }else{
+//            itemArray[indexPath.row].done = false
+//        }
+        
+       
+//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        }else{
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -57,7 +100,10 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add item", style: .default) { (action) in
             
-            self.itemArray.append(textField.text!)
+            let newItem = Items()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             
             self.defaults.setValue(self.itemArray, forKey: "toDolistArray")
             
---
+++ todo/Data Model/Items.swift
@@ -0,0 +1,14 @@
+//
+//  Items.swift
+//  todo
+//
+//  Created by Musa Beni on 08/10/2018.
+//  Copyright © 2018 Musa Beni. All rights reserved.
+//
+
+import Foundation
+
+class Items {
+    var title: String = ""
+    var done:Bool = false
+}
